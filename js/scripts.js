5d87a4723ef929c77de614902a8366f39b8690c4
“Francesca <dascanio.francesca@gmail.com>
2022-12-20 14:38:03 +0100

    Inizializzazione progetto

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*
+    1. Inizio
+    2. Chiedere all'utente numero di chilometri
+    3. Chiedere all'utente età
+    4. Calcolare il prezzo di base
+    5. Controllare se l'utente è <18 anni
+    5a. Applica sconto del 20%
+    6. Controllare se l'utente è >65 anni
+    6a. Applica sconto del 40%
+    7. Output finale: calcolo prezzo in relazione a età e chilometri
+    8. Fine
+*/
